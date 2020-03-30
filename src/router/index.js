f0809666309200d0ced3b8e0ea2c8bbc672dcfd7
lifu <xiaofuai@gmail.com>
2020-03-30 21:41:18 +0800

    添加 router 规则

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,11 @@ const routes = [
     component: Home
   },
   {
+    path: '/city/:city',
+    name: 'Home',
+    component: Home
+  },
+  {
     path: '/about',
     name: 'About',
     // route level code-splitting
